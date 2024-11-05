f242e4c3be9752ee470e0d8557989766e44aa949
fkruklyaramis <francois.krukly@aramis.group>
2024-11-05 14:52:07 +0100

    change main.py - Standard Python

--- projet2/main.py
+++ projet2/main.py
@@ -1,4 +1,8 @@
 from app.code_scrap_book import iterate_categories_print_all_books_pictures
-                              
-iterate_categories_print_all_books_pictures("http://books.toscrape.com/index.html")
 
+def main():
+    url = "http://books.toscrape.com/index.html"
+    iterate_categories_print_all_books_pictures(url)
+
+if __name__ == "__main__":
+    main()
