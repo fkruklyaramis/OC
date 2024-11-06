2cb17c731ca539048dfeaa0cebf3f1478a820859
fkruklyaramis <110470969+fkruklyaramis@users.noreply.github.com>
2024-11-06 09:49:49 +0100

    Update main.py - set url

--- projet2/main.py
+++ projet2/main.py
@@ -1,8 +1,7 @@
 from app.code_scrap_book import iterate_categories_print_all_books_pictures
 
 def main():
-    url = "http://books.toscrape.com/index.html"
-    iterate_categories_print_all_books_pictures(url)
+    iterate_categories_print_all_books_pictures("http://books.toscrape.com/index.html")
 
 if __name__ == "__main__":
     main()
